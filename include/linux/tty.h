d65b4fa953a42168c4b1a26e2eb073573892ae56
Qianqiang Liu <qianqiang.liu@163.com>
2025-01-31 23:07:05 +0800

    init console
    Signed-off-by: Qianqiang Liu <qianqiang.liu@163.com>

---
+++ include/linux/tty.h
@@ -0,0 +1,7 @@
+#ifndef _TTY_H
+#define _TTY_H
+
+void con_init(void);
+void tty_init(void);
+
+#endif /* _TTY_H */
--- kernel/main.c
+++ kernel/main.c
@@ -1,7 +1,10 @@
 #define __LIBRARY__
 
+#include <linux/tty.h>
+
 void main(void)
 {
+	tty_init();
 	__asm__ ("int $0x80\n\r"::);
 	__asm__ __volatile__(
 		"loop:\n\r"
